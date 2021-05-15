394285ef89b6e5a2acc88b144afc1155665da173
markosladic <markosladic6@gmail.com>
2021-05-15 23:34:33 +0200

    filled database

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_service(service_id):
     return service_info['Address'], service_info['Port']
 
 
-register_to_consul()
+# register_to_consul()
 
 
 def has_role(arg):
@@ -60,15 +60,37 @@ def decode_token(token):
     return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
 
 
-#@has_role(['shopping_cart', 'user', 'admin'])
-def add_product(user_id, product_id):
-    carts = db.session.query(ShoppingCart).filter_by(user_id=user_id).all()
-    cart = None
-    for c in carts:
-        if c.product_id is None:
-            c.product_id = product_id
-            cart = c
-            break
+def create_shopping_cart(user_id):
+    sc = ShoppingCart(user_id=user_id, status=Status.CREATED, isPriority=False)
+    db.session.add(sc)
+    db.session.commit()
+
+    return {
+        'user_id': user_id
+    }
+
+
+def create_user():
+    user = User(user_id=2, username="GJ")
+    db.session.add(user)
+    db.session.commit()
+
+    return {
+        'user_id': user.user_id
+    }
+
+
+def create_product():
+    product = Product(product_id=1, name="Skopsko", price=100.0, quantity=3, user_id=2)
+    db.session.add(product)
+    db.session.commit()
+
+
+# @has_role(['shopping_cart', 'user', 'admin'])
+def add_product(user_id, product_id): #trebit user_id i status na inventory(nie im prakjame)
+    cart = db.session.query(ShoppingCart).filter_by(user_id=user_id).first()
+    product = db.session.query(Product).filter_by(product_id=product_id).first()
+    product.user_id = cart.user_id
 
     db.session.add(cart)
     db.session.commit()
@@ -77,7 +99,7 @@ def add_product(user_id, product_id):
             'product_id': product_id}
 
 
-#@has_role(['shopping_cart', 'user', 'admin'])
+# @has_role(['shopping_cart', 'user', 'admin'])
 def remove_product(user_id, product_id):
     carts = db.session.query(ShoppingCart).filter_by(user_id=user_id).all()
     cart = None
@@ -94,7 +116,7 @@ def remove_product(user_id, product_id):
             'product_id': product_id}
 
 
-#@has_role(['shopping_cart', 'user', 'admin'])
+# @has_role(['shopping_cart', 'user', 'admin'])
 def list_all_products(user_id):
     carts = db.session.query(ShoppingCart).filter_by(user_id=user_id).all()
     products = []
@@ -110,7 +132,7 @@ def list_all_products(user_id):
     return products
 
 
-#@has_role(['shopping_cart', 'user', 'admin'])
+# @has_role(['shopping_cart', 'user', 'admin'])
 def change_quantity(user_id, product_id, quantity):
     cart = db.session.query(ShoppingCart).filter_by(user_id=user_id).first()
     product = None
@@ -126,7 +148,7 @@ def change_quantity(user_id, product_id, quantity):
     }
 
 
-#@has_role(['shopping_cart', 'user', 'admin'])
+# @has_role(['shopping_cart', 'user', 'admin'])
 def buy_products(user_id):
     carts = db.session.query(ShoppingCart).filter_by(user_id=user_id).all()
     price = 0
@@ -139,7 +161,7 @@ def buy_products(user_id):
     }
 
 
-#@has_role(['shopping_cart', 'user', 'admin', 'invoices'])
+# @has_role(['shopping_cart', 'user', 'admin', 'invoices'])
 def create_invoice(user_id, transaction_id):
     products = list_all_products(user_id)
     return {'transaction_id': transaction_id,
@@ -154,9 +176,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 connexion_app.add_api("api.yml")
 
-from models import ShoppingCart, Product, Status
+from models import ShoppingCart, Product, Status, User
 
-register_to_consul()
+# register_to_consul()
 
 if __name__ == "__main__":
     connexion_app.run(host='0.0.0.0', port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import relationship
+
 from app import db
 from enum import Enum
 
@@ -10,10 +12,11 @@ class Status(Enum):
 
 class ShoppingCart(db.Model):
     __tablename__ = 'shopping_cart'
-    user_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
     status = db.Column(db.Enum(Status), nullable=False)
     isPriority = db.Column(db.Boolean, nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False)
+    product_id = relationship("Product")
+    user = relationship("User")
 
 
 class User(db.Model):
@@ -28,3 +31,4 @@ class Product(db.Model):
     name = db.Column(db.String, nullable=False)
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('shopping_cart.user_id'), nullable=False)
